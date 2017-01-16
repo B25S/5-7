e0be93fc61c6b1dcebbbf57be26819964fa4f94e
King Yang Q <KingYangQ@alpha.lftltd.net>
2017-01-16 20:17:19 +0800

    add folders

--- py/ReadIndex.py
+++ py/ReadIndex.py
@@ -1,13 +1,16 @@
-fw = open('..\Index_gai.html','wb')
+class readfile:
+	def __init__(self,filename):
+		self.fn = filename
+	def dosomething(fn):
+		fw = open('..\Index.html','wb')
 
-f = open('..\Index.html','r')
-for line in f:
-	if line.find("article-content"):
-		l = line.replace("article-content","content")
-		print l
-		fw.write(l)
-	else:
-		print "o_O!!!"
-f.close()
-fw.close()
-
+		f = open('..\'.append(fn),'r')
+		for line in f:
+			if line.find("article-content"):
+				l = line.replace("article-content","content")
+				print l
+				fw.write(l)
+			else:
+				print "o_O!!!"
+		f.close()
+		fw.close()
